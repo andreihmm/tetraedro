889775e52affd36b56a12f760a89cadb476b2c98
Andrei Alves Fracalossi <124471216+andreihmm@users.noreply.github.com>
2026-03-13 08:49:17 -0300

    Add files via upload

--- manager.cs
+++ manager.cs
@@ -2,23 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class manager : MonoBehaviour {
+
+public class manager : MonoBehaviour
+{
+
+    Vector3 p0 = new Vector3(0, 0, 0);
+    Vector3 p1 = new Vector3(1, 0, 0);
+    Vector3 p2 = new Vector3(0.5f, 0, Mathf.Sqrt(0.75f));
+    Vector3 p3 = new Vector3(0.5f, Mathf.Sqrt(0.75f), Mathf.Sqrt(0.75f) / 3);
 
     public GameObject tetrahedron; // prefab da camrera
     public GameObject[] vetGameObj = new GameObject[24];
     GameObject pai;
-     Vector3 m_Center;
-	// Use this for initialization
-	void Start () {
-		for(int i=0; i < 24; i++)
+    Vector3 m_Center;
+    GameObject v0;
+    GameObject v1;
+    GameObject v2;
+    GameObject v3;
+    // Use this for initialization
+
+    /*void RotacionarCamada(Vector3 verticeAlvo, List<GameObject> pecasDacamada,float angulo)
+    {
+        GameObject pivot = new GameObject("tempPivot");
+        pivot.transform.position = m_center;
+
+        Vector3 eixo = (verticeAlvo - m_Center).normalized;
+
+    }*/
+    void Start()
+    {
+        for (int i = 0; i < 24; i++)
         {
-            if(i == 0)
+
+            if (i == 0)
             {
+
                 vetGameObj[i] = Instantiate(tetrahedron, new Vector3(0, 0, 0), Quaternion.identity); // tetraedro base
+                vetGameObj[i].name = "Tetra_" + i;
+                Debug.Log(i + " -> " + vetGameObj[i].transform.position);
+
             }
             else
-                vetGameObj[i]= Instantiate(tetrahedron, new Vector3(vetGameObj[i-1].transform.position.x + 1, 0, 0), vetGameObj[i - 1].transform.rotation);
-            //i-1 posicao anterior
+            {
+
+                vetGameObj[i] = Instantiate(tetrahedron, new Vector3(vetGameObj[i - 1].transform.position.x + 1, 0, 0), vetGameObj[i - 1].transform.rotation);
+                vetGameObj[i].name = "Tetra_" + i;
+                Debug.Log(i + " -> " + vetGameObj[i].transform.position);
+
+            }//i-1 posicao anterior
         }
 
 
@@ -111,26 +142,30 @@ public class manager : MonoBehaviour {
 
 
         pai.transform.rotation = Quaternion.identity;
-        vetGameObj[21].transform.position = new Vector3(0.5f * 3, 0.86603f, 0.28868f);
-        pai.transform.position = new Vector3(0.5f * 4, 0.86603f, 0.28868f + Mathf.Sqrt(3) / 2);
+        vetGameObj[21].transform.position = new Vector3(0.5f * 3, 0.86603f * 2, -0.28868f);
+        pai.transform.position = new Vector3(2f, 0.86603f * 2, 0.28868f * 2);
         pai.transform.Rotate(Vector3.up, 60f);
         vetGameObj[21].transform.parent = pai.transform;
         pai.transform.Rotate(Vector3.forward, 180f);
         pai.transform.Rotate(Vector3.right, 36.87f);
         vetGameObj[21].transform.parent = null;
 
+
         pai.transform.rotation = Quaternion.identity;
-        vetGameObj[22].transform.position = new Vector3(0.5f * 3, 0.86603f * 2, -0.28868f);
-        pai.transform.position = new Vector3(2f, 0.86603f * 2, 0.28868f * 2);
-        pai.transform.Rotate(Vector3.up, 60f);
-        vetGameObj[22].transform.parent = pai.transform;
-        pai.transform.Rotate(Vector3.forward, 180f);
-        pai.transform.Rotate(Vector3.right, 36.87f);
-        //vetGameObj[22].transform.parent = null;
 
 
 
 
+        Vector3 centro = ((p0 + p1 + p2 + p3) / 4) * 3;
+
+        pai.transform.position = centro;
+
+        v0 = new GameObject();
+        v0.transform.position = new Vector3(0, 0, 0);
+
+        v1 = new GameObject();
+        v1.transform.position = new Vector3(3, 0, 0);
+       
 
         //vetGameObj[3].transform.Rotate(110f,0f,0); // 90f
         // vetGameObj[3].transform.RotateAround(transform.position, Vector3.forward, 5f);
@@ -139,12 +174,58 @@ public class manager : MonoBehaviour {
         //pai.transform.position = new Vector3(0, 1, 0); //pivo
         //vetGameObj[3].transform.parent = pai.transform;
         //vetGameObj[3].transform.bounds
+
     }
 
 
     // Update is called once per frame
-    void Update () {
-		//vetGameObj[3].transform.RotateAround(transform.position, Vector3.forward, 5f);
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            pai.transform.rotation = Quaternion.identity;
+            vetGameObj[9].transform.parent = pai.transform;
+            pai.transform.Rotate(Vector3.up, 60f);
+            vetGameObj[9].transform.parent = null;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            Vector3 direcao = v0.transform.position - pai.transform.position;
+            direcao = direcao.normalized;
+
+            // Desenha o vetor na Scene
+            Debug.DrawRay(pai.transform.position, direcao * 5f, Color.red, 5f);
+
+            pai.transform.rotation = Quaternion.LookRotation(direcao);
+            vetGameObj[0].transform.SetParent(pai.transform);
+
+            pai.transform.Rotate(Vector3.forward, 60f);
+            vetGameObj[0].transform.parent = null;
+
+            Debug.Log("f2.");
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            Vector3 direcao = v1.transform.position - pai.transform.position;
+            direcao = direcao.normalized;
+
+            // Desenha o vetor na Scene
+            Debug.DrawRay(pai.transform.position, direcao * 5f, Color.red, 5f);
+
+            pai.transform.rotation = Quaternion.LookRotation(direcao);
+            vetGameObj[2].transform.SetParent(pai.transform);
+
+            pai.transform.Rotate(Vector3.forward, 60f);
+            vetGameObj[2].transform.parent = null;
+
+            Debug.Log("f2.");
+        }
+
+
+        //vetGameObj[3].transform.RotateAround(transform.position, Vector3.forward, 5f);
         //cria um gameobject: Pai. Tem eixo de rotacao
         //por o objeto como filho deste gameobject
         //rotaciona o gameObjet(pai): consequencia o filho rotaciona
@@ -155,5 +236,5 @@ public class manager : MonoBehaviour {
 
 
         //vetGameObj[4].transform.Rotate((Vector3.right + Vector3.up) * 5);
-	}
+    }
 }
