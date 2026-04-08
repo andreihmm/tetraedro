4e2f4b425340b01e0dcd51ede769671665ecabc0
Andrei Alves Fracalossi <124471216+andreihmm@users.noreply.github.com>
2026-04-08 17:37:44 -0300

    Add files via upload

--- manager.cs
+++ manager.cs
@@ -23,6 +23,8 @@ public class manager : MonoBehaviour
     GameObject v1;
     GameObject v2;
     GameObject v3;
+    int[] tetraNoBaricentro;
+
     // Use this for initialization
 
     /*void RotacionarCamada(Vector3 verticeAlvo, List<GameObject> pecasDacamada,float angulo)
@@ -72,6 +74,7 @@ public class manager : MonoBehaviour
 
         vetGameObj[4].transform.position = new Vector3(0.5f * 3, 0.86603f, 0.28868f);
         gameObjBaricentro[4].transform.position = baricentro(vetGameObj[4]);
+        vetGameObj[4].transform.parent = gameObjBaricentro[4].transform;
 
         vetGameObj[5].transform.position = new Vector3(0.5f, 0, Mathf.Sqrt(3) / 2);
         gameObjBaricentro[5].transform.position = baricentro(vetGameObj[5]);
@@ -87,13 +90,14 @@ public class manager : MonoBehaviour
 
         vetGameObj[8].transform.position = new Vector3(0.5f * 2, 0.86603f, 0.28868f + Mathf.Sqrt(3) / 2);
         gameObjBaricentro[8].transform.position = baricentro(vetGameObj[8]);
+        vetGameObj[8].transform.parent = gameObjBaricentro[8].transform;
 
         vetGameObj[9].transform.position = new Vector3(0.5f * 2, 0.86603f * 2, 0.28868f * 2);
         gameObjBaricentro[9].transform.position = baricentro(vetGameObj[9]);
+        vetGameObj[9].transform.parent = gameObjBaricentro[9].transform;
 
 
         // INVERTIDOS 
-
 
         vetGameObj[10].transform.position = new Vector3(0.5f * 3, 0.86603f, 0.28868f);
         vetGameObj[10].transform.Rotate(Vector3.right, 36.87f);
@@ -101,20 +105,17 @@ public class manager : MonoBehaviour
         gameObjBaricentro[10].transform.position = baricentro(vetGameObj[10]);
         vetGameObj[10].transform.parent = gameObjBaricentro[10].transform;
 
-
         vetGameObj[11].transform.position = new Vector3(0.5f * 5, 0.86603f, 0.28868f);
         vetGameObj[11].transform.Rotate(Vector3.right, 36.87f);
         vetGameObj[11].transform.Rotate(Vector3.forward, 180f);
         gameObjBaricentro[11].transform.position = baricentro(vetGameObj[11]);
         vetGameObj[11].transform.parent = gameObjBaricentro[11].transform;
 
-
         vetGameObj[12].transform.position = new Vector3(0.5f * 4, 0.86603f * 2, 0.28868f * 2);
         vetGameObj[12].transform.Rotate(Vector3.right, 36.87f);
         vetGameObj[12].transform.Rotate(Vector3.forward, 180f);
         gameObjBaricentro[12].transform.position = baricentro(vetGameObj[12]);
-
-
+        vetGameObj[12].transform.parent = gameObjBaricentro[12].transform;
 
         vetGameObj[13].transform.position = new Vector3(1.5f, 0, Mathf.Sqrt(3) / 2);
         vetGameObj[13].transform.Rotate(Vector3.up, 180f);
@@ -141,7 +142,6 @@ public class manager : MonoBehaviour
         gameObjBaricentro[16].transform.position = baricentro(vetGameObj[16]);
         vetGameObj[16].transform.parent = gameObjBaricentro[16].transform;
 
-
         pai.transform.rotation = Quaternion.identity;
         vetGameObj[17].transform.position = new Vector3(0f, 0.86603f, -0.28868f * 2);
         pai.transform.position = new Vector3(0.5f, 0.86603f, 0.28868f);
@@ -162,6 +162,7 @@ public class manager : MonoBehaviour
         pai.transform.Rotate(Vector3.right, 36.87f);
         vetGameObj[18].transform.parent = null;
         gameObjBaricentro[18].transform.position = baricentro(vetGameObj[18]);
+        vetGameObj[18].transform.parent = gameObjBaricentro[18].transform;
 
         // OUTRO LADO!!!!!
 
@@ -176,7 +177,6 @@ public class manager : MonoBehaviour
         gameObjBaricentro[19].transform.position = baricentro(vetGameObj[19]);
         vetGameObj[19].transform.parent = gameObjBaricentro[19].transform;
 
-
         pai.transform.rotation = Quaternion.identity;
         vetGameObj[20].transform.position = new Vector3(0.5f * 3, 0.86603f, 0.28868f);
         pai.transform.position = new Vector3(0.5f * 4, 0.86603f, 0.28868f + Mathf.Sqrt(3) / 2);
@@ -188,7 +188,6 @@ public class manager : MonoBehaviour
         gameObjBaricentro[20].transform.position = baricentro(vetGameObj[20]);
         vetGameObj[20].transform.parent = gameObjBaricentro[20].transform;
 
-
         pai.transform.rotation = Quaternion.identity;
         vetGameObj[21].transform.position = new Vector3(0.5f * 3, 0.86603f * 2, -0.28868f);
         pai.transform.position = new Vector3(2f, 0.86603f * 2, 0.28868f * 2);
@@ -198,7 +197,7 @@ public class manager : MonoBehaviour
         pai.transform.Rotate(Vector3.right, 36.87f);
         vetGameObj[21].transform.parent = null;
         gameObjBaricentro[21].transform.position = baricentro(vetGameObj[21]);
-
+        vetGameObj[21].transform.parent = gameObjBaricentro[21].transform;
 
         pai.transform.rotation = Quaternion.identity;
 
@@ -227,6 +226,13 @@ public class manager : MonoBehaviour
         //vetGameObj[3].transform.parent = pai.transform;
         //vetGameObj[3].transform.bounds
 
+        tetraNoBaricentro = new int[gameObjBaricentro.Length];
+
+        for (int i = 0; i < gameObjBaricentro.Length; i++)
+        {
+            tetraNoBaricentro[i] = i;
+        }
+
     }
 
     Vector3 baricentro(GameObject tetraedro)
@@ -235,6 +241,64 @@ public class manager : MonoBehaviour
         return tetraedro.transform.TransformPoint(centroLocal);
     }
 
+    void ReatribuirBaricentros(int[] origem, int[] destino)
+    {
+        int[] temp = new int[origem.Length];
+
+        // guarda quais tetraedros estavam nas posições de origem
+        for (int i = 0; i < origem.Length; i++)
+        {
+            temp[i] = tetraNoBaricentro[origem[i]];
+        }
+
+        // move logicamente os tetraedros para os novos baricentros
+        for (int i = 0; i < destino.Length; i++)
+        {
+            tetraNoBaricentro[destino[i]] = temp[i];
+        }
+
+        // atualiza a hierarquia real no Unity
+        for (int i = 0; i < destino.Length; i++)
+        {
+            int tetra = temp[i];
+            int baricentroDestino = destino[i];
+
+            vetGameObj[tetra].transform.SetParent(gameObjBaricentro[baricentroDestino].transform, true);
+        }
+    }
+
+
+    void RotacaoLogica(int[] origem, int[] destino)
+    {
+        if (origem.Length != destino.Length)
+        {
+            Debug.LogError("origem e destino precisam ter o mesmo tamanho.");
+            return;
+        }
+
+        int[] ocupantesAntigos = new int[destino.Length];
+
+        // agora lê quem está no destino
+        for (int i = 0; i < destino.Length; i++)
+        {
+            ocupantesAntigos[i] = tetraNoBaricentro[destino[i]];
+        }
+
+        // e coloca na origem
+        for (int i = 0; i < origem.Length; i++)
+        {
+            tetraNoBaricentro[origem[i]] = ocupantesAntigos[i];
+        }
+
+        // atualiza a hierarquia
+        for (int i = 0; i < origem.Length; i++)
+        {
+            int tetra = ocupantesAntigos[i];
+            int novoBaricentro = origem[i];
+
+            vetGameObj[tetra].transform.SetParent(gameObjBaricentro[novoBaricentro].transform, true);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -243,152 +307,225 @@ public class manager : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.F1))
         {
-            //Vector3 direcao = vetGameObj[0].transform.position - pai.transform.position;
-            Vector3 direcao = baricentro(vetGameObj[0]) - pai.transform.position;
-            direcao = direcao.normalized;
-            Debug.Log(direcao);
+            int indiceBaricentro = 0;
+            int tetra = tetraNoBaricentro[indiceBaricentro];
 
-            // Desenha o vetor na Scene
+            Vector3 direcao = baricentro(vetGameObj[tetra]) - pai.transform.position;
+            direcao = direcao.normalized;
+
+            Debug.Log(direcao);
             Debug.DrawRay(pai.transform.position, direcao * 5f, Color.red, 5f);
 
             pai.transform.rotation = Quaternion.LookRotation(direcao);
-            vetGameObj[0].transform.SetParent(pai.transform);
+            vetGameObj[tetra].transform.SetParent(pai.transform, true);
 
-            pai.transform.Rotate(Vector3.forward, 120f);
-            vetGameObj[0].transform.parent = null;
+            pai.transform.Rotate(Vector3.forward, 120f, Space.Self);
+            vetGameObj[tetra].transform.SetParent(gameObjBaricentro[indiceBaricentro].transform, true);
 
             Debug.Log("f1.");
         }
 
-
         if (Input.GetKeyDown(KeyCode.F2))
         {
-            //Vector3 direcao = vetGameObj[2].transform.position - pai.transform.position;
-            Vector3 direcao = baricentro(vetGameObj[2]) - pai.transform.position;
+            int indiceBaricentro = 2;
+            int tetra = tetraNoBaricentro[indiceBaricentro];
+
+            Vector3 direcao = baricentro(vetGameObj[tetra]) - pai.transform.position;
             direcao = direcao.normalized;
 
-            // Desenha o vetor na Scene
             Debug.DrawRay(pai.transform.position, direcao * 5f, Color.red, 5f);
 
             pai.transform.rotation = Quaternion.LookRotation(direcao);
-            vetGameObj[2].transform.SetParent(pai.transform);
+            vetGameObj[tetra].transform.SetParent(pai.transform, true);
 
-            pai.transform.Rotate(Vector3.forward, 120f);
-            vetGameObj[2].transform.parent = null;
+            pai.transform.Rotate(Vector3.forward, 120f, Space.Self);
+            vetGameObj[tetra].transform.SetParent(gameObjBaricentro[indiceBaricentro].transform, true);
 
             Debug.Log("f2.");
         }
 
         if (Input.GetKeyDown(KeyCode.F3))
         {
+            int indiceBaricentro = 9;
+            int tetra = tetraNoBaricentro[indiceBaricentro];
 
             pai.transform.rotation = Quaternion.identity;
-            vetGameObj[9].transform.parent = pai.transform;
-            pai.transform.Rotate(Vector3.up, 120f);
-            vetGameObj[9].transform.parent = null;
+            vetGameObj[tetra].transform.SetParent(pai.transform, true);
+
+            pai.transform.Rotate(Vector3.up, 120f, Space.Self);
+            vetGameObj[tetra].transform.SetParent(gameObjBaricentro[indiceBaricentro].transform, true);
+
+            Debug.Log("f3.");
         }
 
         if (Input.GetKeyDown(KeyCode.F4))
         {
-            Vector3 direcao = baricentro(vetGameObj[7]) - pai.transform.position;
+            int indiceBaricentro = 7;
+            int tetra = tetraNoBaricentro[indiceBaricentro];
+
+            Vector3 direcao = baricentro(vetGameObj[tetra]) - pai.transform.position;
             direcao = direcao.normalized;
 
-            // Desenha o vetor na Scene
             Debug.DrawRay(pai.transform.position, direcao * 5f, Color.red, 5f);
 
             pai.transform.rotation = Quaternion.LookRotation(direcao);
-            vetGameObj[7].transform.SetParent(pai.transform);
+            vetGameObj[tetra].transform.SetParent(pai.transform, true);
 
-            pai.transform.Rotate(Vector3.forward, 120f);
-            vetGameObj[7].transform.parent = null;
+            pai.transform.Rotate(Vector3.forward, 120f, Space.Self);
+            vetGameObj[tetra].transform.SetParent(gameObjBaricentro[indiceBaricentro].transform, true);
 
             Debug.Log("f4.");
         }
-
         // ROTACIONAR BASE
 
         if (Input.GetKeyDown(KeyCode.F5))
         {
             int[] tetraedros_base = { 0, 1, 2, 5, 6, 7, 10, 11, 13, 14, 15, 16, 17, 19, 20 };
-            GameObject[] auxiliar = { };
 
             pai.transform.rotation = Quaternion.identity;
+
             for (int i = 0; i < tetraedros_base.Length; i++)
             {
-                auxiliar[i] = gameObjBaricentro[i];
-
-                // MODIFICAR QUAL TETRAEDRO É FILHO DE QUAL BARICENTRO COM BASE NA ÚLTIMA ROTAÇÃO
-                vetGameObj[tetraedros_base[i]].transform.SetParent(pai.transform);
+                int tetra = tetraNoBaricentro[tetraedros_base[i]];
+                vetGameObj[tetra].transform.SetParent(pai.transform, true);
             }
-            pai.transform.Rotate(Vector3.up, 120f);
 
-            vetGameObj[tetraedros_base[0]].transform.parent = gameObjBaricentro[];
-            vetGameObj[tetraedros_base[0]].transform.parent = gameObjBaricentro[];
-            vetGameObj[tetraedros_base[0]].transform.parent = gameObjBaricentro[];
-            vetGameObj[tetraedros_base[0]].transform.parent = gameObjBaricentro[];
-            vetGameObj[tetraedros_base[0]].transform.parent = gameObjBaricentro[];
-            vetGameObj[tetraedros_base[0]].transform.parent = gameObjBaricentro[];
-            vetGameObj[tetraedros_base[0]].transform.parent = gameObjBaricentro[];
-            vetGameObj[tetraedros_base[0]].transform.parent = gameObjBaricentro[];
-            vetGameObj[tetraedros_base[0]].transform.parent = gameObjBaricentro[];
-            vetGameObj[tetraedros_base[0]].transform.parent = gameObjBaricentro[];
-            vetGameObj[tetraedros_base[0]].transform.parent = gameObjBaricentro[];
-            vetGameObj[tetraedros_base[0]].transform.parent = gameObjBaricentro[];
-            vetGameObj[tetraedros_base[0]].transform.parent = gameObjBaricentro[];
-            vetGameObj[tetraedros_base[0]].transform.parent = gameObjBaricentro[];
-            vetGameObj[tetraedros_base[0]].transform.parent = gameObjBaricentro[];
+            pai.transform.Rotate(Vector3.up, 120f, Space.Self);
+
+            // exemplo de mapeamento entre baricentros
+            int[] origem = { 0, 1, 2, 5, 6, 7, 10, 11, 13, 14, 15, 16, 17, 19, 20 };
+            int[] destino = { 2, 6, 7, 1, 5, 0, 19, 20, 14, 15, 13, 10, 11, 16, 17};
+
+            RotacaoLogica(origem, destino);
         }
 
         // ROTACIONAR 7 TETRAEDRINHOS
 
         if (Input.GetKeyDown(KeyCode.F6))
         {
-            int[] tetraedros_base = { 1, 2, 4, 6, 11, 14, 19 };
+            int[] origem = { 1, 2, 4, 6, 11, 14, 19 };
+            int[] destino = { 6, 2, 1, 4, 14, 19, 11 }; // ajuste manualmente como quiser
 
-            Vector3 direcao = baricentro(vetGameObj[2]) - pai.transform.position;
+            Vector3 direcao = baricentro(vetGameObj[tetraNoBaricentro[2]]) - pai.transform.position;
             direcao = direcao.normalized;
 
             Debug.DrawRay(pai.transform.position, direcao * 5f, Color.red, 5f);
 
             pai.transform.rotation = Quaternion.LookRotation(direcao);
 
-            for (int i = 0; i < tetraedros_base.Length; i++)
+            // parentear no objeto "pai" os tetraedros que atualmente ocupam esses baricentros
+            for (int i = 0; i < origem.Length; i++)
             {
-                vetGameObj[tetraedros_base[i]].transform.SetParent(pai.transform);
+                int tetra = tetraNoBaricentro[origem[i]];
+                vetGameObj[tetra].transform.SetParent(pai.transform, true);
             }
 
-            pai.transform.Rotate(Vector3.forward, 120f);
+            pai.transform.Rotate(Vector3.forward, 120f, Space.Self);
 
-            for (int i = 0; i < tetraedros_base.Length; i++)
+            // lógica invertida: conteúdo de destino vai para origem
+            int[] ocupantesAntigos = new int[destino.Length];
+
+            for (int i = 0; i < destino.Length; i++)
             {
-                vetGameObj[tetraedros_base[i]].transform.parent = null;
+                ocupantesAntigos[i] = tetraNoBaricentro[destino[i]];
+            }
 
+            for (int i = 0; i < origem.Length; i++)
+            {
+                tetraNoBaricentro[origem[i]] = ocupantesAntigos[i];
+            }
+
+            for (int i = 0; i < origem.Length; i++)
+            {
+                int tetra = ocupantesAntigos[i];
+                int novoBaricentro = origem[i];
+
+                vetGameObj[tetra].transform.SetParent(gameObjBaricentro[novoBaricentro].transform, true);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.F7))
+        {
+            int[] origem = { 0, 1, 3, 5, 10, 13, 17 };
+            int[] destino = { 0, 3, 5, 1, 17, 10, 13 }; // ajuste manualmente como desejar
+
+            Vector3 direcao = baricentro(vetGameObj[tetraNoBaricentro[0]]) - pai.transform.position;
+            direcao = direcao.normalized;
+
+            Debug.DrawRay(pai.transform.position, direcao * 5f, Color.red, 5f);
+
+            pai.transform.rotation = Quaternion.LookRotation(direcao);
+
+            // coloca no pai os tetraedros que atualmente ocupam esses baricentros
+            for (int i = 0; i < origem.Length; i++)
+            {
+                int tetra = tetraNoBaricentro[origem[i]];
+                vetGameObj[tetra].transform.SetParent(pai.transform, true);
+            }
+
+            pai.transform.Rotate(Vector3.forward, 120f, Space.Self);
+
+            // lógica invertida: conteúdo de destino vai para origem
+            int[] ocupantesAntigos = new int[destino.Length];
+
+            for (int i = 0; i < destino.Length; i++)
+            {
+                ocupantesAntigos[i] = tetraNoBaricentro[destino[i]];
+            }
+
+            for (int i = 0; i < origem.Length; i++)
+            {
+                tetraNoBaricentro[origem[i]] = ocupantesAntigos[i];
+            }
+
+            for (int i = 0; i < origem.Length; i++)
+            {
+                int tetra = ocupantesAntigos[i];
+                int novoBaricentro = origem[i];
+
+                vetGameObj[tetra].transform.SetParent(gameObjBaricentro[novoBaricentro].transform, true);
             }
         }
 
-
-        if (Input.GetKeyDown(KeyCode.F7))
+        if (Input.GetKeyDown(KeyCode.F8))
         {
-            int[] tetraedros_base = { 0, 1, 3, 5, 10, 13, 17 };
+            int[] origem = { 5, 6, 7, 8, 15, 16, 20 };
+            int[] destino = { 8, 5, 7, 6, 16, 20, 15 }; // ajuste manualmente como desejar
 
-            Vector3 direcao = baricentro(vetGameObj[0]) - pai.transform.position;
+            Vector3 direcao = baricentro(vetGameObj[tetraNoBaricentro[7]]) - pai.transform.position;
             direcao = direcao.normalized;
 
             Debug.DrawRay(pai.transform.position, direcao * 5f, Color.red, 5f);
 
             pai.transform.rotation = Quaternion.LookRotation(direcao);
 
-            for (int i = 0; i < tetraedros_base.Length; i++)
+            // coloca no pai os tetraedros que atualmente ocupam esses baricentros
+            for (int i = 0; i < origem.Length; i++)
             {
-                vetGameObj[tetraedros_base[i]].transform.SetParent(pai.transform);
+                int tetra = tetraNoBaricentro[origem[i]];
+                vetGameObj[tetra].transform.SetParent(pai.transform, true);
             }
 
-            pai.transform.Rotate(Vector3.forward, 120f);
+            pai.transform.Rotate(Vector3.forward, 120f, Space.Self);
 
-            for (int i = 0; i < tetraedros_base.Length; i++)
+            // lógica invertida: conteúdo de destino vai para origem
+            int[] ocupantesAntigos = new int[destino.Length];
+
+            for (int i = 0; i < destino.Length; i++)
             {
-                vetGameObj[tetraedros_base[i]].transform.parent = null;
+                ocupantesAntigos[i] = tetraNoBaricentro[destino[i]];
+            }
 
+            for (int i = 0; i < origem.Length; i++)
+            {
+                tetraNoBaricentro[origem[i]] = ocupantesAntigos[i];
+            }
+
+            for (int i = 0; i < origem.Length; i++)
+            {
+                int tetra = ocupantesAntigos[i];
+                int novoBaricentro = origem[i];
+
+                vetGameObj[tetra].transform.SetParent(gameObjBaricentro[novoBaricentro].transform, true);
             }
         }
 
